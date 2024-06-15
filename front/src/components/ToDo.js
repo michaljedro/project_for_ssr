05427942fe52fef2e2a386df3f3a22b73cf78ca0
Michał Jędro <michal.jedro@wp.pl>
2024-06-15 23:29:39 +0200

    changes in files

--- front/src/components/ToDo.js
+++ front/src/components/ToDo.js
@@ -1,23 +1,10 @@
 import React, { useContext, useRef } from "react";
-import { TodoContext } from "../Context/ToDoContext";
-import {
-  ToDoItem,
-  ToDoBox,
-  InputTodo,
-  LabelTodo,
-  SpanTodo,
-} from "../styles/ToDo.styled";
+import {  ToDoBox} from "../styles/ToDo.styled";
 import Checkbox from "./Checkbox";
 import { ThemeContext } from "../Context/ThemeStore";
 
 function ToDo({ data }) {
-  const { deleteTodo } = useContext(TodoContext);
-  const checkboxRef = useRef();
   const { theme, setTheme } = useContext(ThemeContext);
-
-  const handleChange = (e) => {
-    console.log(e.target.checked);
-  };
 
   return (
     <ToDoBox theme={theme}>
